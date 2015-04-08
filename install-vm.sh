fc9a0907879ebec5b37d6b1713d866b4909b1293
Yoann <yoann.renard@affiliatewindow.com>
2015-04-08 12:40:55 +0100

    install vm script

---
+++ install-vm.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+@ var
+user=yoann
+github_account=YoanRenard-awin
+vm=yoannr
+
+
+sudo mkdir /home/sites/
+sudo chown $user /home/sites/
+cd /home/sites
+
+git clone git@github.com:$github_account/awin-darwin.git
+git clone git@github.com:$github_account/awin-services.git
+git clone git@github.com:$github_account/awin-localization.git
+git clone git@github.com:$github_account/awin-images.git
+git clone git@github.com:$github_account/awin-site2.git
+
+awin-darwin/composer install
+awin-services/composer install
+
+rsync -r -t --progress -v --cvs-exclude awin-darwin/ admin@yoannr.dev.affiliatewindow.com:/home/sites/darwin.affiliatewindow.com/
+rsync -r -t --progress -v --cvs-exclude awin-services/ admin@yoannr.dev.affiliatewindow.com:/home/sites/awin-services/
+rsync -r -t --progress -v --cvs-exclude awin-localization/ admin@yoannr.dev.affiliatewindow.com:/home/sites/localization/
+
