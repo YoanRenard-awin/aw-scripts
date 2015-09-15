97dac8db891da662dc870b4e5fb89d6dc0c19eaf
Yoann <yoann.renard@affiliatewindow.com>
2015-09-15 14:25:03 +0100

    improve generatePublisherDashboardTable.sh. Create table only if not exists

--- generatePublisherDashboardTable.sh
+++ generatePublisherDashboardTable.sh
@@ -24,37 +24,37 @@ do
     yesterdayLastMonthYear=`date -d "1 month ago $i days" +%Y`;
 
     add=""
-    add="$add CREATE TABLE agg_product_count_date_$yesterdayYear.agg_$yesterdayDate$ext                 LIKE agg_product_count_date_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_product_count_date_$todayYear.agg_$todayDate                             LIKE agg_product_count_date_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_product_count_date_$yesterdayYear.agg_$yesterdayDate$ext                 LIKE agg_product_count_date_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_product_count_date_$todayYear.agg_$todayDate                             LIKE agg_product_count_date_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_transaction_date_$yesterdayYear.agg_$yesterdayDate$ext                   LIKE agg_transaction_date_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_transaction_date_$todayYear.agg_$todayDate                               LIKE agg_transaction_date_$refYear.agg_$refDate;\n"
-    add="$add CREATE TABLE agg_transaction_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate$ext LIKE agg_transaction_date_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_transaction_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate     LIKE agg_transaction_date_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_transaction_date_$yesterdayYear.agg_$yesterdayDate$ext                   LIKE agg_transaction_date_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_transaction_date_$todayYear.agg_$todayDate                               LIKE agg_transaction_date_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_transaction_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate$ext LIKE agg_transaction_date_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_transaction_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate     LIKE agg_transaction_date_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_tithe_date_$yesterdayYear.agg_$yesterdayDate$ext                         LIKE agg_tithe_date_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_tithe_date_$todayYear.agg_$todayDate                                     LIKE agg_tithe_date_$refYear.agg_$refDate;\n"
-    add="$add CREATE TABLE agg_tithe_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate$ext       LIKE agg_tithe_date_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_tithe_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate           LIKE agg_tithe_date_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_tithe_date_$yesterdayYear.agg_$yesterdayDate$ext                         LIKE agg_tithe_date_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_tithe_date_$todayYear.agg_$todayDate                                     LIKE agg_tithe_date_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_tithe_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate$ext       LIKE agg_tithe_date_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_tithe_date_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate           LIKE agg_tithe_date_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_click_$yesterdayYear.agg_$yesterdayDate$ext                              LIKE agg_click_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_click_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate$ext            LIKE agg_click_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_click_$todayYear.agg_$todayDate                                          LIKE agg_click_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_click_$yesterdayYear.agg_$yesterdayDate$ext                              LIKE agg_click_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_click_$yesterdayLastMonthYear.agg_$yesterdayLastMonthDate$ext            LIKE agg_click_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_click_$todayYear.agg_$todayDate                                          LIKE agg_click_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_impression_$yesterdayYear.agg_$yesterdayDate$ext                         LIKE agg_impression_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_impression_$todayYear.agg_$todayDate                                     LIKE agg_impression_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_impression_$yesterdayYear.agg_$yesterdayDate$ext                         LIKE agg_impression_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_impression_$todayYear.agg_$todayDate                                     LIKE agg_impression_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_influenced_transaction_$yesterdayYear.agg_$yesterdayDate$ext             LIKE agg_influenced_transaction_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_influenced_transaction_$todayYear.agg_$todayDate                         LIKE agg_influenced_transaction_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_influenced_transaction_$yesterdayYear.agg_$yesterdayDate$ext             LIKE agg_influenced_transaction_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_influenced_transaction_$todayYear.agg_$todayDate                         LIKE agg_influenced_transaction_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_assisted_transaction_$yesterdayYear.agg_$yesterdayDate$ext               LIKE agg_assisted_transaction_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_assisted_transaction_$todayYear.agg_$todayDate                           LIKE agg_assisted_transaction_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_assisted_transaction_$yesterdayYear.agg_$yesterdayDate$ext               LIKE agg_assisted_transaction_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_assisted_transaction_$todayYear.agg_$todayDate                           LIKE agg_assisted_transaction_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_transaction_comdate_$yesterdayYear.agg_$yesterdayDate$ext                LIKE agg_transaction_comdate_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_transaction_comdate_$todayYear.agg_$todayDate                            LIKE agg_transaction_comdate_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_transaction_comdate_$yesterdayYear.agg_$yesterdayDate$ext                LIKE agg_transaction_comdate_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_transaction_comdate_$todayYear.agg_$todayDate                            LIKE agg_transaction_comdate_$refYear.agg_$refDate;\n"
 
-    add="$add CREATE TABLE agg_tithe_comdate_$yesterdayYear.agg_$yesterdayDate$ext                      LIKE agg_tithe_comdate_$refYear.agg_$refDate$ext;\n"
-    add="$add CREATE TABLE agg_tithe_comdate_$todayYear.agg_$todayDate                                  LIKE agg_tithe_comdate_$refYear.agg_$refDate;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_tithe_comdate_$yesterdayYear.agg_$yesterdayDate$ext                      LIKE agg_tithe_comdate_$refYear.agg_$refDate$ext;\n"
+    add="$add CREATE TABLE IF NOT EXISTS agg_tithe_comdate_$todayYear.agg_$todayDate                                  LIKE agg_tithe_comdate_$refYear.agg_$refDate;\n"
 
     sql=$sql$add
 done
